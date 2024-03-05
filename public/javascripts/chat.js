1fa0a668c82d06b8d7118a1436d6290868fb8f5c
Ken Huang <kenhuang@uw.edu>
2024-03-05 11:30:05 -0800

    started chat

---
+++ public/javascripts/chat.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+    document.getElementById("home-btn").addEventListener("click", function() {
+        window.location.href = '/';
+    });
+
+}
