1504c8c9eaec37c83496d731bfb544eafd868900
Ken Huang <kenhuang@uw.edu>
2024-03-05 21:26:15 -0800

    removed chat feature

--- public/javascripts/chat.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", init);
-
-async function init() {
-    document.getElementById("home-btn").addEventListener("click", function() {
-        window.location.href = '/';
-    });
-}
